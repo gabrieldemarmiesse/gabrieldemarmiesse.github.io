824fd8fbea43051c72ae7fc09a9df40b4813625b
gabrieldemarmiesse <gabrieldemarmiesse@hotmail.fr>
2016-11-10 18:48:18 +0100

    Added some first website
    Basic html

---
+++ myScript.js
@@ -0,0 +1,5 @@
+function add_number() {
+    var first_number = parseInt(document.getElementById("Text1").value);
+    var result = first_number;
+    document.getElementById("txtresult").value = result;
+}
