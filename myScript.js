3d579bf019f39708e1cfaf1ae8c6897c49703e3c
gabrieldemarmiesse <gabrieldemarmiesse@hotmail.fr>
2016-11-11 23:20:38 +0100

    Added some comments, semicolons and var.

--- myScript.js
+++ myScript.js
@@ -1,6 +1,8 @@
-function add_number() {
-    var first_number = parseInt(document.getElementById("Text1").value);
-    var result = segmentedSieve(first_number);
+
+// Gets called when the user click on the button
+function event1() {
+    var userInput = parseInt(document.getElementById("Text1").value);
+    var result = primeCountingFunction(userInput, "segmentedSieve");
     document.getElementById("txtresult").value = result;
 }
 
@@ -9,64 +11,90 @@ function sieveOfEratosthenes(n){
 	
 	// Initialisation of our list.
 	// isPrime[a] is true if we think at the current time that a is prime.
-	var isPrime = [false,false]
+	var isPrime = [false,false];
 	for (i = 2; i < n; i++) { 
-		isPrime.push(true)
-		
+		isPrime.push(true);
 	}
-	// We loop until sqrt(n) because of our inner loop which begin at i*i
-	for (i = 2; i <= Math.sqrt(n); i++) { 
+	
+	// We can put all the prime numbers that we find in a list.
+	var primeNumbers = [];
+	
+	// We loop until sqrt(n) because of our inner loop which begins at i*i.
+	for (var i = 2; i <= Math.sqrt(n); i++) { 
 		if(isPrime[i] === true){
+			
+			// We know that now i is a prime number.
+			primeNumbers.push(i);
+			
+			// Display the prime number in the console for debugging if needed.
+			console.log(i);
 			
 			// We remove all the numbers we can 
 			// by multiplying i
-			for(j = i*i; j<n; j = j+i){
-				isPrime[j] = false
+			for(var j = i*i; j<n; j = j+i){
+				isPrime[j] = false;
 			}
 		}
 	}
 	
-	// Now we can put all the prime numbers in a list
-	primeNumbers = []
-	for(i = 2; i<n; i++){
+	
+	// We stopped at sqrt(n), now we must collect the remaining results.
+	for(var i = Math.ceil(Math.sqrt(n)); i<n; i++){
 		
 		// isPrime can now tell us if i is prime
 		if(isPrime[i] === true){
-			primeNumbers.push(i)
-			console.log(i)
+			primeNumbers.push(i);
+			
+			// Display the prime number in the console for debugging if needed.
+			console.log(i);
 		}
 	}
-	return primeNumbers
-}
-
-function primeCountingFunction(n){
-	return sieveOfEratosthenes(n).length
+	return primeNumbers;
 }
 
 
+// Returns the number of primes before n using either 
+// sieveOfEratosthenes or segmentedSieve
+function primeCountingFunction(n, algorithme){
+	
+	switch (algorithme){
+		case "sieveOfEratosthenes":
+			return sieveOfEratosthenes(n).length;
+			break;
+			
+		case "segmentedSieve":
+			return segmentedSieve(n);
+			break;
+			
+		default:
+			console.log("The algorithme does not exist.");
+	}
+}
 
 
 // A more memory friendly version of the sieve_of_eratosthenes
 // The space complexity is now O(sqrt(n)) instead of O(n)
 // The list "isPrime" is now splitted into smaller lists of size sqrt(n)
 function segmentedSieve(n){
-	isPrimeSize = Math.ceil(Math.sqrt(n));
-	isPrimeNbPieces = Math.ceil(n/isPrimeSize);
 	
-	mainPrimes = sieveOfEratosthenes(isPrimeSize+1);
-	primesCount = mainPrimes.length;
+	// Setting up the segmentation
+	var isPrimeSize = Math.ceil(Math.sqrt(n));
+	var isPrimeNbPieces = Math.ceil(n/isPrimeSize);
+	
+	// Obtaining the primes below sqrt(n)
+	var mainPrimes = sieveOfEratosthenes(isPrimeSize+1);
+	var primesCount = mainPrimes.length;
 	
 	// We are going to increment the multiples of those primes.
 	// currentValues is the list that will remember where we left off.
-	currentValues = []
+	currentValues = [];
 	for (i = 0; i < mainPrimes.length; i++){
 		square = Math.pow(mainPrimes[i], 2);
 		currentValues.push(square);
 	}
 	
-	// We iterate over the pieces of isPrime.
-	// But we're not considering the first one, 
-	// since it was done with the function sieveOfEratosthenes
+	// We iterate over the segments.
+	// j=1 because the first segment was done with the function sieveOfEratosthenes.
 	for (j = 1; j < isPrimeNbPieces; j++){
 		
 		// We create the current piece of isPrime
@@ -75,13 +103,14 @@ function segmentedSieve(n){
 		// Now we loop over the prime numbers.
 		for (i = 0; i < mainPrimes.length; i++){
 			
+			// While the values that we are testing are lower than the current piece's upper bound
 			while (currentValues[i] <= isPrimeSize*(j+1)){
 				isPrimePiece[currentValues[i]-j*isPrimeSize - 1] = false;
 				currentValues[i] += mainPrimes[i];
 			}
 		} 
 		
-		
+		// If it's the last segment, we may need to discard the end of it. 
 		if (j === isPrimeNbPieces - 1){
 			limit = n - isPrimeSize * (isPrimeNbPieces-1) - 1;
 		}
@@ -98,5 +127,5 @@ function segmentedSieve(n){
 		}
 	}
 	
-	return primesCount
+	return primesCount;
 }
